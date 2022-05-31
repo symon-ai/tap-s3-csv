dc3c3e49a515ae2c31c975deb9cd50e4b787bff1
jay-varicent <jama@varicent.com>
2022-05-31 09:42:10 -0400

    WP-10258 fix issues for using tap-s3-csv for csv imports

--- tap_s3_csv/csv_iterator.py
+++ tap_s3_csv/csv_iterator.py
@@ -18,16 +18,20 @@ def get_row_iterator(iterable, options=None):
     headers = set(reader.fieldnames)
 
     # Check for duplicate columns
+    fieldname_pool = set()
+    duplicate_cols = set()
     if len(reader.fieldnames) != len(headers):
-        fieldnames = set()
-        duplicate_cols = set()
         for fieldname in reader.fieldnames:
-            if fieldname in fieldnames:
+            if fieldname == '':
+                continue
+            fieldname = fieldname.casefold()
+            if fieldname in fieldname_pool:
                 duplicate_cols.add(fieldname)
             else:
-                fieldnames.add(fieldname)
+                fieldname_pool.add(fieldname)
 
-        raise Exception('CSV file contains duplicate columns: {}'.format(duplicate_cols))
+        if len(duplicate_cols) > 0:
+            raise Exception('CSV file contains duplicate columns: {}'.format(duplicate_cols))
 
     if options.get('key_properties'):
         key_properties = set(options['key_properties'])
@@ -41,4 +45,32 @@ def get_row_iterator(iterable, options=None):
             raise Exception('CSV file missing date_overrides headers: {}, file only contains headers for fields: {}'
                             .format(date_overrides - headers, headers))
 
+    final_fieldnames = handle_empty_fieldnames(reader.fieldnames, fieldname_pool, options)
+
+    setattr(reader, '_fieldnames', final_fieldnames)
+
     return reader
+
+def handle_empty_fieldnames(fieldnames, fieldname_pool, options):
+    quotechar = options.get('quotechar', '"')
+    delimiter = options.get('delimiter', ',')
+    is_external = options.get('is_external',  True)
+
+    auto_generate_header_num = 0
+    final_fieldnames = []
+    for fieldname in fieldnames:
+        # handle edge case uncovered in WP-9886 for csv import
+        # import is from csv connector (not s3 connector) if it is_external is set to False
+        if not is_external and fieldname and delimiter in fieldname:
+            fieldname = quotechar + fieldname + quotechar
+                
+        if fieldname == '' or fieldname is None:
+            fieldname = f'col_{auto_generate_header_num}'
+            while fieldname in fieldname_pool:
+                auto_generate_header_num += 1
+                fieldname = f'col_{auto_generate_header_num}'
+            auto_generate_header_num += 1
+        
+        final_fieldnames.append(fieldname)
+    
+    return final_fieldnames
