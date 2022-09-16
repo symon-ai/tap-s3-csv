06519869ac15eb440b0d8b4cfed745b4ee74fae7
yi-variceht <kliu@varicent.com>
2022-09-16 16:22:23 -0400

    undo

--- tap_s3_csv/sync.py
+++ tap_s3_csv/sync.py
@@ -202,7 +202,6 @@ def sync_compressed_file(config, s3_path, table_spec, stream):
     return records_streamed
 
 def sync_csv_file(config, file_handle, s3_path, table_spec, stream, timers={}):
-    start = time.time()
     LOGGER.info('Syncing file "%s".', s3_path)
 
     table_name = table_spec['table_name']
