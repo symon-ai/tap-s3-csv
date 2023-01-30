b7c61e4a1b28441629301fc2b76f9bc8079b42fe
Mitchell Dooreleyers <mdooreleyers@varicent.com>
2023-01-30 11:27:51 -0500

    WP-13911 Remove unnecessary functions

--- tap_s3_csv/conversion.py
+++ tap_s3_csv/conversion.py
@@ -6,52 +6,6 @@ LOGGER = singer.get_logger()
 
 # pylint: disable=too-many-return-statements
 
-
-def infer(key, datum, date_overrides, check_second_call=False):
-    """
-    Returns the inferred data type
-    """
-    if datum is None or datum == '':
-        return None
-
-    try:
-        if isinstance(datum, list):
-            data_type = 'string'
-            if check_second_call:
-                LOGGER.warning(
-                    'Unsupported type for "%s", List inside list is not supported hence will be treated as a string', key)
-            elif not datum:
-                data_type = 'list'
-            else:
-                inferred = infer(key, datum[0], date_overrides, True)
-                # default it to string if element inside datum list is None
-                if inferred is None: 
-                    inferred = 'string'
-                data_type = 'list.' + inferred
-            return data_type
-
-        if key in date_overrides:
-            return 'date-time'
-
-        if isinstance(datum, dict):
-            return 'dict'
-
-        try:
-            int(str(datum))
-            return 'integer'
-        except (ValueError, TypeError):
-            pass
-        try:
-            float(str(datum))
-            return 'number'
-        except (ValueError, TypeError):
-            pass
-
-    except (ValueError, TypeError):
-        pass
-
-    return 'string'
-
 def infer_column(column, dateFormatMap, lengths):
     if column.isnull().all():
         lengths[column.name] = 0
@@ -156,7 +110,6 @@ def infer_boolean(column):
 
 
 def is_boolean_value(value):
-
     if value == 'True' or value == 'False':
         return True
 
@@ -167,56 +120,6 @@ def is_boolean_value(value):
         return True
 
     return False
-
-
-def process_sample(sample, counts, lengths, table_spec):
-    for key, value in sample.items():
-        if key not in counts:
-            counts[key] = {}
-
-        length = len(value) if value is not None else 0
-        if key not in lengths or length > lengths[key]:
-            lengths[key] = length
-
-        date_overrides = table_spec.get('date_overrides', [])
-        datatype = infer(key, value, date_overrides)
-
-        if datatype is not None:
-            counts[key][datatype] = counts[key].get(datatype, 0) + 1
-
-    return counts, lengths
-
-
-def pick_datatype(counts):
-    """
-    If the underlying records are ONLY of type `integer`, `number`,
-    or `date-time`, then return that datatype.
-
-    If the underlying records are of type `integer` and `number` only,
-    return `number`.
-
-    Otherwise return `string`.
-    """
-    to_return = 'string'
-
-    list_of_datatypes = ['list.date-time', 'list.dict', 'list.integer',
-                         'list.number', 'list.string', 'list', 'date-time', 'dict']
-
-    for data_types in list_of_datatypes:
-        if counts.get(data_types, 0) > 0:
-            return data_types
-
-    if len(counts) == 1:
-        if counts.get('integer', 0) > 0:
-            to_return = 'integer'
-        elif counts.get('number', 0) > 0:
-            to_return = 'number'
-    elif(len(counts) == 2 and
-         counts.get('integer', 0) > 0 and
-         counts.get('number', 0) > 0):
-        to_return = 'number'
-
-    return to_return
 
 
 def generate_schema(samples, table_spec, string_max_length: bool):
