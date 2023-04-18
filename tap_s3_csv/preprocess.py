383355d51ada447238bd90a233fd1e4a4ed84014
jay-varicent <jama@varicent.com>
2023-04-17 20:52:21 -0400

    WP-15206 fix fieldname error with special chars

--- tap_s3_csv/preprocess.py
+++ tap_s3_csv/preprocess.py
@@ -1,4 +1,6 @@
 from queue import Queue
+import csv
+import codecs
 
 # Wrapper class for file streams. Handles preprocessing (skipping header rows, footer rows, detecting headers)
 class PreprocessStream():
@@ -19,38 +21,58 @@ class PreprocessStream():
             has_header = table_spec.get('has_header', True)
             encoding = table_spec.get('encoding', 'utf-8')
             delimiter = table_spec.get('delimiter', ',')
-            self._handle_first_row(has_header, encoding, delimiter)
+            quotechar = table_spec.get('quotechar', '"')
+            escapechar = table_spec.get('escape_char', '\\')
+            self._handle_first_row(has_header, encoding, delimiter, quotechar, escapechar)
     
     def _skip_header_rows(self, skip_header_row):
         try:
             for _ in range(skip_header_row):
                 next(self.file_iterator)
         except StopIteration:
-            raise Exception(f'preprocess_err: No more data after skipping rows in header.')
-
-    def _skip_empty_rows(self):
-        try:
-            first_row = next(self.file_iterator)
-            while first_row == b'' or first_row == b'\n':
-                first_row = next(self.file_iterator)
-        except StopIteration:
-            raise Exception(f'preprocess_err: No more data other than empty rows.')
-        return first_row
+            raise Exception(f'preprocess_err: We can’t find any data after the skipped rows in the header.')
     
     # grabs first non empty row and process it as header row or first record row depending on has_header
-    def _handle_first_row(self, has_header, encoding, delimiter):
-        first_row = self._skip_empty_rows()
-        first_row_list = first_row.decode(encoding).split(delimiter)
+    def _handle_first_row(self, has_header, encoding, delimiter, quotechar, escapechar):
+        # Use csv.DictReader to parse first row and use it as header if has_header == True, else 
+        # use it to detect the number of columns and generate headers. We need to use csv.DictReader
+        # for parsing first row to handle corner cases such as:
+        # - fields in first row contain newline char wrapped with quotechar or escaped with escapechar
+        # - fields in first row contain delimiter wrapped with quotechar or escaped with escapechar
+        file_stream = codecs.iterdecode(
+            self.iter_lines(), encoding=encoding, errors='replace')
+        reader = csv.DictReader(
+            (line.replace('\0', '') for line in file_stream),
+            fieldnames=None,
+            delimiter=delimiter,
+            escapechar=escapechar,
+            quotechar=quotechar)
         
+        if reader.fieldnames is None:
+            raise Exception('File is empty.')
+
+        first_row_parsed = reader.fieldnames
+
         # first row is header row
         if has_header:
-            self.header = first_row_list
+            self.header = first_row_parsed
             return
         
-        # first row is a record, generate headers and store first row so we can yield it in iter_lines
-        self.first_row = first_row
-        fieldnames = [f'col_{i}' for i in range(len(first_row_list))]
-        self.header = fieldnames
+        # first row is a record, generate headers 
+        self.header = [f'col_{i}' for i in range(len(first_row_parsed))]
+        
+        # first row has been parsed into array of string, change it back to byte form to yield in iter_lines.
+        first_row_str_form = ''
+        for field in first_row_parsed:
+            # Each field has been parsed into final value by csv.Dictreader after handling delimiters, quotechars and escapechars.
+            # Since csv.DictReader uses quotechar to quote fields containing special chars, such as the delimiter/quote char/newline char,
+            # we can simply wrap fields with quotechar so that csv.DictReader can later parse them into same value.
+            # Make sure to escape escapechars and quotechars that are actually part of fieldname
+            field = field.replace(escapechar, escapechar * 2).replace(quotechar, escapechar + quotechar)
+            first_row_str_form += quotechar + field + quotechar + delimiter
+        encoder = codecs.getincrementalencoder(encoding)()
+        first_row_byte_form = encoder.encode(first_row_str_form[:-1])
+        self.first_row = first_row_byte_form
 
     def iter_lines(self):
         if self.first_row is not None:
