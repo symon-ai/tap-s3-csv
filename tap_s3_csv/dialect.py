995cd7a971022661a4962799c8075d72445d2bdb
Octavio Percivaldi <opercivaldi@varicent.com>
2025-12-18 12:48:41 -0300

    WP-28565: Removed MAX_LINE_BYTES

--- tap_s3_csv/dialect.py
+++ tap_s3_csv/dialect.py
@@ -8,7 +8,6 @@ import clevercsv
 from clevercsv.dialect import SimpleDialect
 
 from tap_s3_csv import s3, preprocess
-from tap_s3_csv.symon_exception import SymonException
 
 # We started using tap_s3_csv in 3.4 for both s3 and csv imports. Dialect detection
 # is only run for csv imports
@@ -49,9 +48,6 @@ def detect_dialect(config, s3_file, table):
     # max bytes we want to cache in memory
     MAX_LINES_BYTES = 25 * 1024 ** 2 # 25MB
 
-    # max bytes for each line read
-    MAX_LINE_BYTES = 1024 ** 2 # 1MB
-
     # chardet is slow and rarely detects early. We limit the number of lines it is fed to keep performance acceptable.
     # The question is how many lines and how do we pick the most interesting lines?
     #
@@ -83,13 +79,6 @@ def detect_dialect(config, s3_file, table):
         try:
             line = next(file_iter)
             line_bytes = len(line)
-
-            if line_bytes >= MAX_LINE_BYTES:
-                max_size_mb = MAX_LINE_BYTES / (1024 ** 2)
-                raise SymonException(
-                    f'Row exceeds maximum allowed size of {max_size_mb:.0f}MB. Please reduce the size of individual rows in your file.',
-                    'RowSizeLimitExceeded'
-                )
 
             lines_read += 1
             if bytes_read + line_bytes <= MAX_LINES_BYTES:
