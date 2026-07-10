5b9235fef779956c04e6fd1e6bafd136f15ed45a
dgomez5342 <dgomez@varicent.com>
2026-07-10 16:04:15 -0400

    Update __init__.py

--- tap_s3_csv/__init__.py
+++ tap_s3_csv/__init__.py
@@ -265,12 +265,9 @@ def main():
             elif args.properties:
                 do_sync(config, args.properties, args.state)
         except ClientError as e:
-            if external_source:
-                symon_error = s3.build_symon_exception_from_client_error(e, config.get('bucket'))
-                if symon_error is e:
-                    raise
-                raise symon_error from e
-            raise
+            if not external_source:
+                raise
+            raise s3.build_symon_exception_from_client_error(e, config.get('bucket')) from e
     except SymonException as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         error_info = {
